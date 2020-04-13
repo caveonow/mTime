e2bcc8a408de0c0f0785f7ad65e2941ad2a5701a
Henly Ng <henly_cy@hotmail.com>
2020-04-13 23:56:52 +0800

    CY 20200413 Shift - update Shift feature and UI

--- mTime/mTime/Controllers/ShiftController.vb
+++ mTime/mTime/Controllers/ShiftController.vb
@@ -18,13 +18,27 @@ Namespace Controllers
 
         ' GET : Create-Shift
         Function Create() As ActionResult
-            Return View()
+            Dim Shift As New model.SHIFT
+
+            Shift.ISWORKDAY1 = False
+            Shift.ISWORKDAY2 = False
+            Shift.ISWORKDAY3 = False
+            Shift.ISWORKDAY4 = False
+            Shift.ISWORKDAY5 = False
+            Shift.ISWORKDAY6 = False
+            Shift.ISWORKDAY7 = False
+
+            Shift.ISFLEXIHOUR = True
+
+            Return View(Shift)
         End Function
 
         <HttpPost>
         <ValidateAntiForgeryToken>
         Function Create(ByVal Shift As model.SHIFT) As ActionResult
+            ValidateBeforeSave(Shift)
 
+            ' Check is duplicate
             Dim blnIsDuplicated As Boolean
 
             blnIsDuplicated = db.SHIFT.Any(Function(model) model.SHIFTID = Shift.SHIFTID)
@@ -34,6 +48,7 @@ Namespace Controllers
             End If
 
             If ModelState.IsValid Then
+                Shift = SetupBeforeSave(Shift)
 
                 Shift.ISINUSED = True
                 Shift.CREATEDON = Now
@@ -90,18 +105,23 @@ Namespace Controllers
 
         <HttpPost>
         <ValidateAntiForgeryToken>
-        Function Edit(<Bind(Include:="SHIFTID, 
-                FLEXISTARTTIMEFROM1, FLEXISTARTTIMETO1, WORKHOUR1, ISOFFDAY1, 
-                FLEXISTARTTIMEFROM2, FLEXISTARTTIMETO2, WORKHOUR2, ISOFFDAY2, 
-                FLEXISTARTTIMEFROM3, FLEXISTARTTIMETO3, WORKHOUR3, ISOFFDAY3, 
-                FLEXISTARTTIMEFROM4, FLEXISTARTTIMETO4, WORKHOUR4, ISOFFDAY4, 
-                FLEXISTARTTIMEFROM5, FLEXISTARTTIMETO5, WORKHOUR5, ISOFFDAY5, 
-                FLEXISTARTTIMEFROM6, FLEXISTARTTIMETO6, WORKHOUR6, ISOFFDAY6, 
-                FLEXISTARTTIMEFROM7, FLEXISTARTTIMETO7, WORKHOUR7, ISOFFDAY7, 
+        Function Edit(<Bind(Include:="SHIFTID, ISFLEXIHOUR,
+                TIMEINSTART1, TIMEINEND1, WORKHOUR1, ISWORKDAY1, 
+                TIMEINSTART2, TIMEINEND2, WORKHOUR2, ISWORKDAY2, 
+                TIMEINSTART3, TIMEINEND3, WORKHOUR3, ISWORKDAY3, 
+                TIMEINSTART4, TIMEINEND4, WORKHOUR4, ISWORKDAY4, 
+                TIMEINSTART5, TIMEINEND5, WORKHOUR5, ISWORKDAY5, 
+                TIMEINSTART6, TIMEINEND6, WORKHOUR6, ISWORKDAY6, 
+                TIMEINSTART7, TIMEINEND7, WORKHOUR7, ISWORKDAY7, 
                 GRACEPERIODFORLATEIN, GRACEPERIODFOREARLYOUT, REMARK,
                 ISINUSED, CREATEDBY, CREATEDON, UPDAEDBY, UPDATEDBY")> ByVal Shift As model.SHIFT) As ActionResult
 
+            ValidateBeforeSave(Shift)
+
             If ModelState.IsValid Then
+                ' update time in end 
+                Shift = SetupBeforeSave(Shift)
+                
                 Shift.UPDATEDBY = "SYSTEM"
                 Shift.UPDATEDON = Now
 
@@ -143,5 +163,107 @@ Namespace Controllers
             ViewBag.Result = "OK"
             Return View(Shift)
         End Function
+
+        Function ValidateBeforeSave(Shift As model.SHIFT)
+            ' check everything again
+            If Shift.ISWORKDAY1 Then
+                If IsNothing(Shift.TIMEINSTART1) Then
+                    ModelState.AddModelError("TIMEINSTART1", "Monday time in start is required")
+                End If
+                If IsNothing(Shift.TIMEINEND1) Then
+                    ModelState.AddModelError("TIMEINEND1", "Monday time in end is required")
+                End If
+                If IsNothing(Shift.WORKHOUR1) Then
+                    ModelState.AddModelError("WORKHOUR1", "Monday working hour start is required")
+                End If
+            End If
+
+            If Shift.ISWORKDAY2 Then
+                If IsNothing(Shift.TIMEINSTART2) Then
+                    ModelState.AddModelError("TIMEINSTART2", "Tuesday time in start is required")
+                End If
+                If IsNothing(Shift.TIMEINEND2) Then
+                    ModelState.AddModelError("TIMEINEND2", "Tuesday time in end is required")
+                End If
+                If IsNothing(Shift.WORKHOUR2) Then
+                    ModelState.AddModelError("WORKHOUR2", "Tuesday working hour start is required")
+                End If
+            End If
+
+            If Shift.ISWORKDAY3 Then
+                If IsNothing(Shift.TIMEINSTART3) Then
+                    ModelState.AddModelError("TIMEINSTART3", "Wednesday time in start is required")
+                End If
+                If IsNothing(Shift.TIMEINEND3) Then
+                    ModelState.AddModelError("TIMEINEND3", "Wednesday time in end is required")
+                End If
+                If IsNothing(Shift.WORKHOUR3) Then
+                    ModelState.AddModelError("WORKHOUR3", "Wednesday working hour start is required")
+                End If
+            End If
+
+            If Shift.ISWORKDAY4 Then
+                If IsNothing(Shift.TIMEINSTART4) Then
+                    ModelState.AddModelError("TIMEINSTART4", "Thursday time in start is required")
+                End If
+                If IsNothing(Shift.TIMEINEND4) Then
+                    ModelState.AddModelError("TIMEINEND4", "Thursday time in end is required")
+                End If
+                If IsNothing(Shift.WORKHOUR4) Then
+                    ModelState.AddModelError("WORKHOUR4", "Thursday working hour start is required")
+                End If
+            End If
+
+            If Shift.ISWORKDAY5 Then
+                If IsNothing(Shift.TIMEINSTART5) Then
+                    ModelState.AddModelError("TIMEINSTART5", "Friday time in start is required")
+                End If
+                If IsNothing(Shift.TIMEINEND5) Then
+                    ModelState.AddModelError("TIMEINEND5", "Friday time in end is required")
+                End If
+                If IsNothing(Shift.WORKHOUR5) Then
+                    ModelState.AddModelError("WORKHOUR5", "Friday working hour start is required")
+                End If
+            End If
+
+            If Shift.ISWORKDAY6 Then
+                If IsNothing(Shift.TIMEINSTART6) Then
+                    ModelState.AddModelError("TIMEINSTART6", "Saturday time in start is required")
+                End If
+                If IsNothing(Shift.TIMEINEND6) Then
+                    ModelState.AddModelError("TIMEINEND6", "Saturday time in end is required")
+                End If
+                If IsNothing(Shift.WORKHOUR6) Then
+                    ModelState.AddModelError("WORKHOUR6", "Saturday working hour start is required")
+                End If
+            End If
+
+            If Shift.ISWORKDAY7 Then
+                If IsNothing(Shift.TIMEINSTART7) Then
+                    ModelState.AddModelError("TIMEINSTART7", "Sunday time in start is required")
+                End If
+                If IsNothing(Shift.TIMEINEND7) Then
+                    ModelState.AddModelError("TIMEINEND7", "Sunday time in end is required")
+                End If
+                If IsNothing(Shift.WORKHOUR7) Then
+                    ModelState.AddModelError("WORKHOUR7", "Sunday working hour start is required")
+                End If
+            End If
+        END Function
+
+        Function SetupBeforeSave(Shift As model.SHIFT) As model.SHIFT
+            ' update time in end 
+            If Not Shift.ISFLEXIHOUR Then
+                Shift.TIMEINEND1 = Shift.TIMEINSTART1
+                Shift.TIMEINEND2 = Shift.TIMEINSTART2
+                Shift.TIMEINEND3 = Shift.TIMEINSTART3
+                Shift.TIMEINEND4 = Shift.TIMEINSTART4
+                Shift.TIMEINEND5 = Shift.TIMEINSTART5
+                Shift.TIMEINEND6 = Shift.TIMEINSTART6
+                Shift.TIMEINEND7 = Shift.TIMEINSTART7
+            End If
+
+            Return Shift
+        END Function
     End Class
 End Namespace
--- mTime/mTime/Models/ShiftModel.vb
+++ mTime/mTime/Models/ShiftModel.vb
@@ -6,62 +6,41 @@ Namespace model
 
         <Required(ErrorMessage:="Shift Code is required")>
         Public Property SHIFTID As String
-        Public Property ISISFLEXIHOUR As Boolean
+        Public Property ISFLEXIHOUR As Boolean
 
-        <Required(ErrorMessage:="Start Time is required")>
-        Public Property TIMEINSTART1 As TimeSpan
-        <Required(ErrorMessage:="End Time is required")>
-        Public Property TIMEINEND1 As TimeSpan
-        <Required(ErrorMessage:="Working Hour is required")>
-        Public Property WORKHOUR1 As Integer
+        Public Property TIMEINSTART1 As Nullable(Of TimeSpan)
+        Public Property TIMEINEND1 As Nullable(Of TimeSpan)
+        Public Property WORKHOUR1 As Nullable(Of Integer)
         Public Property ISWORKDAY1 As Boolean
 
-        <Required(ErrorMessage:="Start Time is required")>
-        Public Property TIMEINSTART2 As TimeSpan
-        <Required(ErrorMessage:="End Time is required")>
-        Public Property TIMEINEND2 As TimeSpan
-        <Required(ErrorMessage:="Working Hour is required")>
-        Public Property WORKHOUR2 As Integer
+        Public Property TIMEINSTART2 As Nullable(Of TimeSpan)
+        Public Property TIMEINEND2 As Nullable(Of TimeSpan)
+        Public Property WORKHOUR2 As Nullable(Of Integer)
         Public Property ISWORKDAY2 As Boolean
 
-        <Required(ErrorMessage:="Start Time is required")>
-        Public Property TIMEINSTART3 As TimeSpan
-        <Required(ErrorMessage:="End Time is required")>
-        Public Property TIMEINEND3 As TimeSpan
-        <Required(ErrorMessage:="Working Hour is required")>
-        Public Property WORKHOUR3 As Integer
+        Public Property TIMEINSTART3 As Nullable(Of TimeSpan)
+        Public Property TIMEINEND3 As Nullable(Of TimeSpan)
+        Public Property WORKHOUR3 As Nullable(Of Integer)
         Public Property ISWORKDAY3 As Boolean
 
-        <Required(ErrorMessage:="Start Time is required")>
-        Public Property TIMEINSTART4 As TimeSpan
-        <Required(ErrorMessage:="End Time is required")>
-        Public Property TIMEINEND4 As TimeSpan
-        <Required(ErrorMessage:="Working Hour is required")>
-        Public Property WORKHOUR4 As Integer
+        Public Property TIMEINSTART4 As Nullable(Of TimeSpan)
+        Public Property TIMEINEND4 As Nullable(Of TimeSpan)
+        Public Property WORKHOUR4 As Nullable(Of Integer)
         Public Property ISWORKDAY4 As Boolean
 
-        <Required(ErrorMessage:="Start Time is required")>
-        Public Property TIMEINSTART5 As TimeSpan
-        <Required(ErrorMessage:="End Time is required")>
-        Public Property TIMEINEND5 As TimeSpan
-        <Required(ErrorMessage:="Working Hour is required")>
-        Public Property WORKHOUR5 As Integer
+        Public Property TIMEINSTART5 As Nullable(Of TimeSpan)
+        Public Property TIMEINEND5 As Nullable(Of TimeSpan)
+        Public Property WORKHOUR5 As Nullable(Of Integer)
         Public Property ISWORKDAY5 As Boolean
 
-        <Required(ErrorMessage:="Start Time is required")>
-        Public Property TIMEINSTART6 As TimeSpan
-        <Required(ErrorMessage:="End Time is required")>
-        Public Property TIMEINEND6 As TimeSpan
-        <Required(ErrorMessage:="Working Hour is required")>
-        Public Property WORKHOUR6 As Integer
+        Public Property TIMEINSTART6 As Nullable(Of TimeSpan)
+        Public Property TIMEINEND6 As Nullable(Of TimeSpan)
+        Public Property WORKHOUR6 As Nullable(Of Integer)
         Public Property ISWORKDAY6 As Boolean
 
-        <Required(ErrorMessage:="Start Time is required")>
-        Public Property TIMEINSTART7 As TimeSpan
-        <Required(ErrorMessage:="End Time is required")>
-        Public Property TIMEINEND7 As TimeSpan
-        <Required(ErrorMessage:="Working Hour is required")>
-        Public Property WORKHOUR7 As Integer
+        Public Property TIMEINSTART7 As Nullable(Of TimeSpan)
+        Public Property TIMEINEND7 As Nullable(Of TimeSpan)
+        Public Property WORKHOUR7 As Nullable(Of Integer)
         Public Property ISWORKDAY7 As Boolean
 
         <Required(ErrorMessage:="Grace Period for Late-In is required")>
